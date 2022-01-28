2424f32f9807f3179f76bf2c74783fbe93d8bfa1
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-28 18:16:19 +0530

    Pushing new project

---
+++ ASPNetIdentityDemo/ASPNetIdentityDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNetIdentityDemo.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateProfile()
+        {
+            return View();
+        }
+    }
+}
